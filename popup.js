67ac6af8cecbc1c7ce82a77fd6537de246578489
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-11 17:52:58 -0400

    settimeout to display popup

--- popup.js
+++ popup.js
@@ -3,7 +3,6 @@ const popup=document.querySelector('.popup-wrapper')
 const closePopup=document.querySelector('.popup-close')
 
 
-
 btn.addEventListener('click', ()=>{
 
     popup.style.display='block'
@@ -14,3 +13,9 @@ closePopup.addEventListener('click', ()=>{
 
     popup.style.display='none'
 })
+
+//set timeout of 5 seconds to display the popup on the screen
+setTimeout(()=>{
+    popup.style.display='block'
+
+}, 5000)
